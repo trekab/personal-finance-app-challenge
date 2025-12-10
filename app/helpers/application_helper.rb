bdae2815a10df9d418bde4c1cba1672cf5fd8ad3
rmatovu987 <rmatovu987@gmail.com>
2025-12-10 06:32:26 +0300

    Work on navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_item(path)
+    is_active = current_page?(path)
+
+    base = "relative flex flex-col items-center gap-1 px-3 pt-2 pb-4 rounded-t flex-1 mx-2"
+    active = "bg-white text-gray-900"
+    inactive = "text-gray-300 hover:bg-gray-800"
+
+    "#{base} #{is_active ? active : inactive}"
+  end
 end
